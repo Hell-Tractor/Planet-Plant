198e0771c98fe122ce9d276f91b94b2ec90283ad
Hell_Tractor <1104840448@qq.com>
2022-05-07 16:40:04 +0800

    Finished skill point dialog show

--- Assets/Scripts/Managers/BlackSceneManager.cs
+++ Assets/Scripts/Managers/BlackSceneManager.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class BlackSceneManager : MonoBehaviour {
+    public GameObject SkillPointDialog = null;
+    
     private DialogManager _dialogManager;
     private void Start() {
         _dialogManager = this.GetComponent<DialogManager>();
+        _dialogManager.ShowDialog(1);
         _dialogManager.OnDialogEnd += (int id) => {
-            SceneManager.LoadScene("Field");
+            SkillPointDialog?.SetActive(true);
+            SkillPointDialog?.GetComponent<SkillPointAllocateManager>()?.ConfirmButton?.GetComponent<Button>()?.onClick.AddListener(() => SceneManager.LoadScene("Field"));
         };
+
     }
 }
