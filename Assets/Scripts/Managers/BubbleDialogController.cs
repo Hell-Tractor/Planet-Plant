86699d15899ec4fcb5814b7e7db040986961234d
Hell_Tractor <1104840448@qq.com>
2022-05-18 17:35:12 +0800

    Finished beginner's guide in field

---
+++ Assets/Scripts/Managers/BubbleDialogController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BubbleDialogController : MonoBehaviour {
+    public Text Text;
+
+    private List<Tuple<string, Func<bool>>> _dialogQueue = new List<Tuple<string, Func<bool>>>();
+    private int? _currentDialog = null;
+    public void AddDialog(string text, Func<bool> check) {
+        _dialogQueue.Add(new Tuple<string, Func<bool>>(text, check));
+    }
+
+    public void Clear() {
+        _dialogQueue.Clear();
+    }
+
+    public void Show() {
+        this.gameObject.SetActive(true);
+        _currentDialog = 0;
+        this.Text.text = _dialogQueue[_currentDialog.Value].Item1;
+    }
+
+    private void Update() {
+        if (_currentDialog != null && _currentDialog < _dialogQueue.Count) {
+            if (_dialogQueue[_currentDialog.Value].Item2()) {
+                if (++_currentDialog == _dialogQueue.Count) {
+                    this.gameObject.SetActive(false);
+                    _currentDialog = null;
+                } else {
+                    this.Text.text = _dialogQueue[_currentDialog.Value].Item1;
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/Managers/FieldManager.cs
+++ Assets/Scripts/Managers/FieldManager.cs
@@ -1,11 +1,75 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class FieldManager : MonoBehaviour {
+    public BubbleDialogController _bubbleDialog;
+
+    private void _setupBeginnerGuide() {
+        _bubbleDialog.Clear();
+
+        _bubbleDialog.AddDialog("鼠标左键点击右上角背包图标打开物品栏", () => {
+            if (Input.GetMouseButtonDown(0)) {
+                List<RaycastResult> hits = new List<RaycastResult>();
+                EventSystem.current.RaycastAll(new PointerEventData(EventSystem.current) {
+                    position = Input.mousePosition
+                }, hits);
+                
+                if (hits.Count == 0)
+                    return false;
+
+                if (hits[0].gameObject.name == "bag") {
+                    return true;
+                }
+
+                return false;
+            }
+            return false;
+        });
+
+        _bubbleDialog.AddDialog("左键点击物品栏中的种子", () => {
+            if (Input.GetMouseButtonDown(0)) {
+                List<RaycastResult> hits = new List<RaycastResult>();
+                EventSystem.current.RaycastAll(new PointerEventData(EventSystem.current) {
+                    position = Input.mousePosition
+                }, hits);
+                
+                foreach (var hit in hits) {
+                    if (hit.gameObject.CompareTag("InventorySlot")) {
+                        Slot slot = hit.gameObject.GetComponent<Slot>();
+                        if (slot?.GetItem()?.Id == "planetplant:seed") {
+                            return true;
+                        }
+                    }
+                }
+
+                return false;
+            }
+            return false;
+        });
+
+        int count = Inventory._itemFollowMouse?.Count ?? 0;
+
+        _bubbleDialog.AddDialog("右键点击要种下的地块（只有左边第一列可种，其他田地需要花钱开垦）", () => {
+            if (Input.GetMouseButtonDown(1)) {
+                var hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 0f, 1 << LayerMask.NameToLayer("Farmland"));
+                return hit.collider?.GetComponent<Farmland>() != null;
+            }
+            return false;
+        });
+
+        _bubbleDialog.AddDialog("种植成功！随时间推移，种植的作物将会成熟，收割后可以出售，是非常重要的经济来源哦~", () => {
+            return Input.GetMouseButtonDown(0);
+        });
+    }
+    
     private void Start() {
         if (GlobalProperties.Instance.isFirstTimeToField) {
             GlobalProperties.Instance.isFirstTimeToField = false;
 
             // DialogManager.Instance.ShowDialog(19);
+            this._setupBeginnerGuide();
+            _bubbleDialog.Show();
         }
 
         TimeManager.Instance.Resume();
