855316d677462410f407401815adc78b0dc353fb
Hell_Tractor <1104840448@qq.com>
2022-05-25 22:51:13 +0800

    Add dialog at home

--- Assets/Scripts/Managers/HomeManager.cs
+++ Assets/Scripts/Managers/HomeManager.cs
@@ -2,20 +2,37 @@ using UnityEngine;
 
 public class HomeManager : MonoBehaviour {
     public BubbleDialogController BubbleDialogController = null;
+    private bool _isAfterMarketDialogShown = false;
     private void Start() {
+        bool bubbleDialogExists = false;
+        
         if (GlobalProperties.Instance.isFirstTimeGoHome) {
             GlobalProperties.Instance.isFirstTimeGoHome = false;
 
-            // DialogManager.Instance.ShowDialog(22);
             _setupBeginnerGuide();
             BubbleDialogController?.Show();
+            bubbleDialogExists = true;
+        }
+
+        if (!_isAfterMarketDialogShown && !bubbleDialogExists) {
+            DialogManager.Instance.ShowDialog(13);
+            _isAfterMarketDialogShown = true;
         }
     }
 
     private void _setupBeginnerGuide() {
         BubbleDialogController?.Clear();
         BubbleDialogController?.AddDialog("每天至少要上交家里1块钱 剩余的钱存起来当做学费 需要在九月开学之前攒够学费", () => {
-            return Input.GetMouseButton(0);
+            if (Input.GetMouseButton(0)) {
+                if (GlobalProperties.Instance.isFristTimeToMarket == false) {
+                    DialogManager.Instance.ShowDialog(13);
+                    _isAfterMarketDialogShown = true;
+                } else {
+                    DialogManager.Instance.ShowDialog(4);
+                }
+                return true;
+            }
+            return false;
         });
     }
 }
